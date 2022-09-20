15c7d0768b5538ced7a177c7ade3d166fbe9f07d
Callee84 <carl.g.holm@gmail.com>
2022-09-20 19:16:45 +0000

    add function to calculate answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,7 +41,16 @@ function checkAnswer() {
 }
 
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
 
+    if (operator === '+') {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`unimplemented operator ${operator}`)
+        throw `unimplemented operator ${operator}, Aborting!`;
+    }
 }
 
 function incrementScore() {
@@ -59,11 +68,15 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion (){
-
+    document.getElementById('operand1').textContent = operand1
+    document.getElementById('operand2').textContent = operand2
+    document.getElementById('operator').textContent = "/"
 }
 
 function displayMultiplyQuestion (){
-
+    document.getElementById('operand1').textContent = operand1
+    document.getElementById('operand2').textContent = operand2
+    document.getElementById('operator').textContent = "*"
 }
 
 function dispalyDivisionAnswer() {
