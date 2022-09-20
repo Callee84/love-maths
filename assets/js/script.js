b61df4ff33f5046646b69e9c0271d66918d0ef85
Callee84 <carl.g.holm@gmail.com>
2022-09-20 21:38:18 +0000

    add code to division

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        dispalyDivisionAnswer(num1, num2);
 
     } else {
         alert(`Unknown game type: ${gameType}`)
@@ -82,6 +84,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") { 
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") { 
+        return [operand1 / operand2, "division"];
     } else {
         alert(`unimplemented operator ${operator}`)
         throw `unimplemented operator ${operator}, Aborting!`;
@@ -132,7 +136,7 @@ function displayMultiplyQuestion (operand1, operand2){
 
 function dispalyDivisionAnswer(operand1, operand2) {
     
-    document.getElementById('operand1').textContent = operand1
-    document.getElementById('operand2').textContent = operand2
-    document.getElementById('operator').textContent = "/"
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2; 
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/" 
 }
