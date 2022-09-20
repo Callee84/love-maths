218d7c19b686fc814a048f7b9a92de3cc82f7907
Callee84 <carl.g.holm@gmail.com>
2022-09-20 19:38:31 +0000

    add code to check answer and respod to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(){
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -37,7 +37,15 @@ function runGame(gameType) {
 
 
 function checkAnswer() {
+    let userAnswer =parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
+    if (isCorrect) {
+        alert("Yey! You got it right!")
+    } else {
+        alert(`Awwww... You answered ${userAnswer}. The correct answer is ${calculatedAnswer[0]}!`);
+    }
 }
 
 function calculateCorrectAnswer() {
